aad7311ad3a2cd4b162455de617d49adeb453455
Nico Diaz <nicodiazan@gmail.com>
2025-03-19 21:47:29 +0100

    Change the text displayed when getting the bare adress

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(express.json());
 
 app.get("/", (req, res) => {
-	res.send("Hello World");
+	res.send("Welcome to the Digimon API");
 });
 
 app.get("/digimons", async (req, res) => {
