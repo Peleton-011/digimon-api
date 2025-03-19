73e659434fded044ab7496cc284d4bba7da09c7d
Nico Diaz <nicodiazan@gmail.com>
2025-03-19 21:35:36 +0100

    Add a general digimons endpoint

--- app.js
+++ app.js
@@ -1,7 +1,20 @@
 const express = require("express");
+const { getDigimons } = require("./dynamo");
 const app = express();
 app.get("/", (req, res) => {
 	res.send("Hello World");
+});
+
+app.get("/digimons", async (req, res) => {
+	try {
+		const digimons = await getDigimons();
+		res.json(digimons);
+	} catch (error) {
+		console.error(error);
+		res.status(500).json({
+			error: "Something went wrong: " + error.message,
+		});
+	}
 });
 
 const port = process.env.PORT || 3000;
